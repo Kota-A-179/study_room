7f0b7e968ef380fca488a5b8dd48ce4e18829979
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-11 18:16:45 +0900

    マイグレーションファイルの修正とusersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_11_090607) do
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "nickname"
+    t.integer "occupation_id", null: false
+    t.integer "icon_id", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.date "birthday", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
