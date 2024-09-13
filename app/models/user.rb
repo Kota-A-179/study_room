069192ef5d5882ba044743ca7ec1d509ccdd12f1
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-13 16:56:13 +0900

    バリデーションんとエラーメッセージの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は英字と数字の両方を含んでください'}, if: -> {password.present?} 
   
-  validates :status_id, presence: true, numericality: { other_than: 1 }
-  validates :icon_id, presence: true, numericality: { other_than: 1 }
+  validates :status_id, presence: true, numericality: { other_than: 1, message: "を選択してください" }
+  validates :icon_id, presence: true, numericality: { other_than: 1, message: "を選択してください" }
   validates :birthday, presence: true
 end
 
