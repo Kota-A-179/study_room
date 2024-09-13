955428527aaccd7dfe79a92bbda14249613ea606
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-13 16:59:45 +0900

    保存機能とリダイレクトの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameter, if: :devise_controller?
+  
+  protected
   def after_sign_in_path_for(resource)
-    room_users_path
+    room_users_path(resource)
+  end
+  
+  def after_sign_up_path_for(resource)
+    room_users_path(resource)
+  end
+
+  private
+  def configure_permitted_parameter
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :status_id, :occupation_id, :icon_id, :birthday])
   end
 end
