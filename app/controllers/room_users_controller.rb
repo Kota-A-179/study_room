a169144e84120f491579302045add4d71abefb0b
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-14 16:20:00 +0900

    一覧画面に対するインスタン変数の用意

--- app/controllers/room_users_controller.rb
+++ app/controllers/room_users_controller.rb
@@ -1,5 +1,7 @@
 class RoomUsersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   def index
+    @room = Room.find(params[:room_id])
+    @room_users = @room.room_users.includes(:user)
   end
 end
