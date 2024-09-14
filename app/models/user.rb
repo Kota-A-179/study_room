94084cd71038bef07c108adcb56cb61d726e7fa2
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-14 16:17:13 +0900

    アソシエーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   belongs_to :occupation
   belongs_to :icon
 
-  has_one :room_users
+  has_one :room_user
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
