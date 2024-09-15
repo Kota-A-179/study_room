801216964e101426719e70fc04391662e809c46b
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-15 23:09:47 +0900

    ログイン時の既存のルームユーザー一覧削除設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   
   protected
   def after_sign_in_path_for(resource)
+    room_user_destroy
     @room = room_search || @room = Room.create(name: "#{Room.count + 1}")
     @room.room_users.create(user_id: resource.id)
     room_room_users_path(room_id: @room.id) 
@@ -17,6 +18,10 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameter
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :status_id, :occupation_id, :icon_id, :birthday])
+  end
+  
+  def room_user_destroy
+    RoomUser.where(user_id: resource.id).destroy_all 
   end
 
   def room_search
