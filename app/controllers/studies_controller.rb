e5fb4a846591cc912a9503f501ad2ff238532b29
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-16 16:31:52 +0900

    編集機能の設定

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -1,12 +1,12 @@
 class StudiesController < ApplicationController
+  before_action :set_room
+
   def new
     @study = current_user.studies.new
-    @room = Room.find(current_user.room_user.room_id)
   end
 
   def create
     @study = current_user.studies.new(study_params)
-    @room = Room.find(current_user.room_user.room_id)
     if @study.save
       redirect_to room_room_users_path(@room)
     else
@@ -14,7 +14,24 @@ class StudiesController < ApplicationController
     end
   end
 
+  def edit
+    @study = current_user.studies.last
+  end
+
+  def update
+    @study = Study.find(params[:id])
+    if @study.update(study_params)
+      redirect_to room_room_users_path(@room)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  private
+  def set_room
+    @room = Room.find(current_user.room_user.room_id)
+  end
+
   def study_params
-    params.require(:study).permit(:title, :goal)
+    params.require(:study).permit(:title, :goal, :content, :review)
   end
 end
