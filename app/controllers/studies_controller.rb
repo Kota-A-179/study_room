f6b8eb62932cb5d6b9087d3755247d6ffe62290c
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-16 15:47:53 +0900

    新規登録機能の設定

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -3,4 +3,18 @@ class StudiesController < ApplicationController
     @study = current_user.studies.new
     @room = Room.find(current_user.room_user.room_id)
   end
+
+  def create
+    @study = current_user.studies.new(study_params)
+    @room = Room.find(current_user.room_user.room_id)
+    if @study.save
+      redirect_to room_room_users_path(@room)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def study_params
+    params.require(:study).permit(:title, :goal)
+  end
 end
