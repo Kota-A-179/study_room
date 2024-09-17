2987cb68d8e70dafd900904ca3de8a3cceccafa9
Kota-A-179 <ando.kota0228@gmail.com>
2024-09-17 16:23:10 +0900

    ログイン後のユーザー情報取得方法の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     room_user_destroy
     @room = room_search || @room = Room.create(name: "#{Room.count + 1}")
-    @room.room_users.create(user_id: resource.id)
+    @room.room_users.create(user_id: current_user.id)
     room_room_users_path(room_id: @room.id) 
   end
   
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   end
   
   def room_user_destroy
-    RoomUser.where(user_id: resource.id).destroy_all 
+    RoomUser.where(user_id: current_user.id).destroy_all 
   end
 
   def room_search
