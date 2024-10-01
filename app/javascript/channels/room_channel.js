74a63ae6125abe7f4b3b7eb5a0787c069362c208
Kota-A-179 <ando.kota0228@gmail.com>
2024-10-01 22:27:44 +0900

    room_channnelの修正

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -2,20 +2,20 @@ import consumer from "channels/consumer"
 
 document.addEventListener("DOMContentLoaded", function() {
   const roomId = document.getElementById("room-id").value;
-});
 
-consumer.subscriptions.create({ channel: "RoomChannel", room_id: roomId }, {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+  consumer.subscriptions.create({ channel: "RoomChannel", room_id: roomId }, {
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
 
-  received(data) {
-    // 受け取ったデータを画面に追加
-    const roomUsersWrap = document.querySelector('.room-users-wrap');
-    roomUsersWrap.insertAdjacentHTML('beforeend', data.user);
-  }
+    received(data) {
+      // 受け取ったデータを画面に追加
+      const roomUsersWrap = document.querySelector('.room-users-wrap');
+      roomUsersWrap.insertAdjacentHTML('beforeend', data.user);
+    }
+  });
 });
