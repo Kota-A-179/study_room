6c769946521a4b7a849526c809f1436beb332b3a
Kota-A-179 <ando.kota0228@gmail.com>
2024-10-02 18:15:12 +0900

    サブスクライブの切り替え設定

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,38 +1,49 @@
 import consumer from "channels/consumer"
 
+let currentRoomId = null;
+let currentSubscription = null;
+
 document.addEventListener("DOMContentLoaded", function() {
   const roomId = document.getElementById("room-id").value;
 
-  consumer.subscriptions.create({ channel: "RoomChannel", room_id: roomId }, {
-    connected() {
-      // Called when the subscription is ready for use on the server
-      console.log("Connected to RoomChannel");
-    },
+  if (currentRoomId !== roomId) {
+    if (currentSubscription) {
+      currentSubscription.unsubscribe();
+    }
 
-    disconnected() {
-      // Called when the subscription has been terminated by the server
-      console.log("Disconnected from RoomChannel");
-    },
+    currentRoomId = roomId;
 
-    received(data) {
-      const roomUsersWrap = document.querySelector('.room-users-wrap');
+    consumer.subscriptions.create({ channel: "RoomChannel", room_id: roomId }, {
+      connected() {
+        // Called when the subscription is ready for use on the server
+        console.log("Connected to RoomChannel");
+      },
 
-      if (data.action == 'add') {
-        // 受け取ったデータを画面に追加
-        roomUsersWrap.insertAdjacentHTML('beforeend', data.user);
-      } else if (data.action == 'remove') {
-        //ユーザーのHTML要素を削除
-        const userElement = document.getElementById(`user-${data.user_id}`);
-        if (userElement) {
-          userElement.remove();
-        }
-      } else if (data.action == 'update') {
-        //ユーザーのHTML要素を更新
-        const userElement = document.getElementById(`user-${data.user_id}`);
-        if (userElement) {
-          userElement.outerHTML = data.html;
+      disconnected() {
+        // Called when the subscription has been terminated by the server
+        console.log("Disconnected from RoomChannel");
+      },
+
+      received(data) {
+        const roomUsersWrap = document.querySelector('.room-users-wrap');
+
+        if (data.action == 'add') {
+          // 受け取ったデータを画面に追加
+          roomUsersWrap.insertAdjacentHTML('beforeend', data.user);
+        } else if (data.action == 'remove') {
+          //ユーザーのHTML要素を削除
+          const userElement = document.getElementById(`user-${data.user_id}`);
+          if (userElement) {
+            userElement.remove();
+          }
+        } else if (data.action == 'update') {
+          //ユーザーのHTML要素を更新
+          const userElement = document.getElementById(`user-${data.user_id}`);
+          if (userElement) {
+            userElement.outerHTML = data.html;
+          }
         }
       }
-    }
-  });
+    });
+  }
 });
