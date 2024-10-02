c7a725b95d114630b57350cbea735b6576d073d3
Kota-A-179 <ando.kota0228@gmail.com>
2024-10-02 17:19:11 +0900

    registrations_controllerのカスタム

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth  
-  before_action :configure_permitted_parameter, if: :devise_controller?
-  protected
- 
-  
-  def after_sign_up_path_for(resource)
-    cookies.encrypted[:user_id] = { value: resource.id, expires: 1.hour.from_now }
-    room_path(resource.room) 
+
+  def update_room_user
+    ActionCable.server.broadcast "room_channel_#{current_user.room.id}", {
+          action: 'update',
+          user_id: current_user.id,
+          html: render_to_string(partial: 'room_user', locals: { user: current_user })
+        }
   end
 
   private
+  
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
-
-  def configure_permitted_parameter
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :status_id, :occupation_id, :icon_id, :goal, :birthday])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :status_id, :occupation_id, :icon_id, :goal, :birthday])
-  end
-  
 end
---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,32 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameter, only: [:create, :update]
+
+  def create
+    super
+  end
+
+  def after_sign_up_path_for(resource)
+    cookies.encrypted[:user_id] = { value: resource.id, expires: 1.hour.from_now }
+    resource.room.present? room_path(resource.room) : room_path
+  end
+
+  def update
+    if current_user.update(user_params)  
+      update_room_user
+      redirect_to user_path(current_user)
+    else
+      render :edit, status: :unprocessable_entity 
+    end
+  end
+
+  private
+  def configure_permitted_parameter
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :status_id, :occupation_id, :icon_id, :goal, :birthday])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :status_id, :occupation_id, :icon_id, :goal, :birthday])
+  end
+
+  def user_params  
+    params.require(:user).permit(:nickname, :status_id, :occupation_id, :icon_id, :goal, :birthday)
+  end
+
+end
